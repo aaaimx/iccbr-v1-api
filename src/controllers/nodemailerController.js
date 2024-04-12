f2a5ae6c0db3bb694a58cdb43fc48f90f5839b84
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-04-12 13:29:58 -0600

    refactor (nodemailer): switch email content from HTML to PDF
        Replace nodemailer function in nodemailerController.js sending HTML content with a new one generating and attaching a PDF containing registration information.

--- src/controllers/nodemailerController.js
+++ src/controllers/nodemailerController.js
@@ -1,94 +1,97 @@
 // Nodemailer transporter
 const createTransporter = require("../config/nodemailer.js");
+const PDFDocument = require("pdfkit");
 
-// Function to build the HTML of the ticket table
-function buildTicketsTableHTML(tickets) {
-  let ticketsTableHTML =
-    "<table border='1'><tr><th>Ticket</th><th>Description</th><th>Quantity</th><th>Price</th><th>Total Cost</th></tr>";
-  let totalCostSum = 0;
+function buildPDFContent(formData, tickets, registrationId) {
+  const doc = new PDFDocument();
+  const chunks = [];
 
-  tickets.forEach((ticket) => {
-    const totalCost = ticket.quantity * ticket.price;
-    totalCostSum += totalCost;
-
-    ticketsTableHTML += `<tr><td>${ticket.name}</td><td>${ticket.description}</td><td>${ticket.quantity}</td><td>${ticket.price}</td><td>${totalCost}</td></tr>`;
-  });
-
-  ticketsTableHTML += "</table>";
-
-  return { ticketsTableHTML, totalCostSum };
-}
-
-// Function to build the HTML of user information
-function buildUserInfoHTML(formData, registrationId) {
-  // Validate payment status
+  // Main information
+  doc.text("Your registration has been successful!", { align: "center" });
+  doc.text(`Registration ID: ${registrationId}`, { align: "center" });
+  doc.moveDown();
+  doc.text("User Information:");
+  doc.text(`First Name: ${formData.firstName}`);
+  doc.text(`Last Name: ${formData.lastName}`);
+  doc.text(`Phone Number: ${formData.mobileNumber}`);
+  doc.text(`Email: ${formData.email}`);
+  doc.text(`Country: ${formData.country}`);
+  doc.text(`Institution: ${formData.institution}`);
+  doc.text(`Address: ${formData.address}`);
+  doc.text(`Payment Method: ${formData.paymentMethod}`);
   const paymentStatus = formData.paymentMethod === "stripe" ? "Paid" : "Unpaid";
+  doc.text(`Payment Status: ${paymentStatus}`);
+  doc.moveDown();
 
-  return `
-    <p><strong>First Name:</strong> ${formData.firstName}</p>
-    <p><strong>Last Name:</strong> ${formData.lastName}</p>
-    <p><strong>Phone Number:</strong> ${formData.mobileNumber}</p>
-    <p><strong>Email:</strong> ${formData.email}</p>
-    <p><strong>Country:</strong> ${formData.country}</p>
-    <p><strong>Institution:</strong> ${formData.institution}</p>
-    <p><strong>Address:</strong> ${formData.address}</p>
-    <p><strong>Payment Method:</strong> ${formData.paymentMethod}</p>
-    <p><strong>Payment Status:</strong> ${paymentStatus}</p>
-  `.trim();
-}
+  // Tickets information
+  doc.text("Tickets Information:");
+  tickets.forEach((ticket) => {
+    doc.text(`Ticket: ${ticket.name}`);
+    doc.text(`Description: ${ticket.description}`);
+    doc.text(`Quantity: ${ticket.quantity}`);
+    doc.text(`Price: ${ticket.price}`);
+    const totalCost = ticket.quantity * ticket.price;
+    doc.text(`Total Cost: ${totalCost}`);
+    doc.moveDown();
+  });
+  const totalCostSum = tickets.reduce(
+    (acc, ticket) => acc + ticket.quantity * ticket.price,
+    0
+  );
+  doc.text(`Total amount: $${totalCostSum}`);
+  doc.moveDown();
 
-// Function to build mail options
-function buildMailOptions(
-  formData,
-  ticketsTableHTML,
-  totalCostSum,
-  registrationId
-) {
-  let paymentInfoHTML = ""; // Initialize an empty string to store the payment information
-
-  // If the payment method is not "stripe", we build the payment information section
+  // Bank Transfer information
   if (formData.paymentMethod !== "stripe") {
-    paymentInfoHTML = `
-      <h2><strong>To make the payment you can do it to this account:</strong></h2>
-      <p><strong>Name account:</strong> Tec Mérida</p>
-      <p><strong>Concept:</strong> 00000</p>
-      <p><strong>Reference:</strong> 00000000</p>
-      <p>Once the transfer has been made, please send the receipt and indicating if you require an invoice to the following email: <a href="mailto:registration@iccbr2024.org">registration@iccbr2024.org</a></p>
-    `.trim();
+    doc.text("To make the payment you can do it to this account:");
+    doc.text("Name account: Tec Mérida");
+    doc.text("Concept: 00000");
+    doc.text("Reference: 00000000");
+    doc.text(
+      "Once the transfer has been made, please send the receipt and indicating if you require an invoice to the following email: registration@iccbr2024.org"
+    );
+    doc.moveDown();
   }
 
-  return {
+  // Manejar la salida del PDF
+  doc.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+
+  doc.on("end", () => {
+    const pdfBuffer = Buffer.concat(chunks);
+    buildMailOptions(formData, pdfBuffer, registrationId);
+  });
+
+  doc.end();
+}
+
+// Función para construir las opciones del correo
+function buildMailOptions(formData, pdfBuffer, registrationId) {
+  const mailOptions = {
     from: process.env.NM_USER,
     to: formData.email,
     subject: "ICCBR 2024 - Successful Registration",
+    attachments: [
+      {
+        filename: `registration_${registrationId}.pdf`,
+        content: pdfBuffer,
+      },
+    ],
     html: `
       <h1>Your registration has been successful!</h1>
 
-      <h2><strong>Registration ID:</strong> ${registrationId}</h2>
-
-      ${buildUserInfoHTML(formData)}
-
-      <h2>Tickets information</h2>
-      ${ticketsTableHTML}
-      <h2><strong>Total amount:</strong> $${totalCostSum}</h2>
-
-      ${paymentInfoHTML}
+      <p>Please, find attached your registation invoice for ICCBR 2024.</p>
 
       <p>If you have any questions or problems with your registration, contact this email: <a href="mailto:registration@iccbr2024.org">registration@iccbr2024.org</a></p>
     `,
   };
+
+  sendEmail(mailOptions);
 }
 
-// Feature to send registration email
-async function sendRegistrationEmail(formData, tickets, registrationId) {
-  const { ticketsTableHTML, totalCostSum } = buildTicketsTableHTML(tickets);
-  const mailOptions = buildMailOptions(
-    formData,
-    ticketsTableHTML,
-    totalCostSum,
-    registrationId
-  );
-
+// Función para enviar el correo de registro
+async function sendEmail(mailOptions) {
   try {
     const transporter = createTransporter();
     await transporter.sendMail(mailOptions);
@@ -97,6 +100,10 @@ async function sendRegistrationEmail(formData, tickets, registrationId) {
     console.error("Error sending registration email:", error);
     throw new Error("Failed to send registration email");
   }
+}
+
+async function sendRegistrationEmail(formData, tickets, registrationId) {
+  buildPDFContent(formData, tickets, registrationId);
 }
 
 module.exports = {
